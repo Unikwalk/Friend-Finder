576327da3149ffb0abb677c6dcb397c4097c990c
Unikwalk <vo.jennifer.n@gmail.com>
2019-03-18 21:41:12 -0700

    testing.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [ {
     "name" : "Ni",
-    "photo" : "",
+    "photo" : "https://www.goldenglobes.com/sites/default/files/articles/cover_images/constance_wu-ag-20180806_zap_g203_037.pre_.jpeg",
     "scores" : [5,4,2,5,1,1,2,1,3,5]
 }, {
     "name" : "Alex",
@@ -20,7 +20,7 @@ var friends = [ {
     "scores" : [4,1,5,3,4,5,2,3,4,5]
 }, {
     "name" : "Brenda",
-    "photo" : "",
+    "photo" : "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsmilingbonita.files.wordpress.com%2F2011%2F04%2F10122501.jpg&imgrefurl=https%3A%2F%2Fsmilingbonita.wordpress.com%2F2011%2F04%2F19%2Fchinese-actress%2F&docid=90V0Ps8l0F2dAM&tbnid=HA2LusUdikTZ7M%3A&vet=10ahUKEwjy97r6nI3hAhUJsp4KHai1ChEQMwiaASg2MDY..i&w=1280&h=960&bih=512&biw=840&q=famous%20chinese%20actress&ved=0ahUKEwjy97r6nI3hAhUJsp4KHai1ChEQMwiaASg2MDY&iact=mrc&uact=8",
     "scores" : [4,1,5,3,4,5,2,3,4,5]
 }, {
     "name" : "Brian",
@@ -44,7 +44,7 @@ var friends = [ {
     "scores" : [5,2,4,3,3,2,1,3,3,4]
 }, {
     "name" : "Sherry",
-    "photo" : "",
+    "photo" : "www.google.com",
     "scores" : [1,1,1,1,1,1,1,1,1,1]
 }, {
     "name" : "Amy",
