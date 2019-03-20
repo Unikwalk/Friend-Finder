0beeaf9dc3a0fbc94a2e6f77ccf499c3e2267e73
Unikwalk <vo.jennifer.n@gmail.com>
2019-03-19 23:45:19 -0700

    Fixed url for images and scores.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,59 +1,59 @@
 var friends = [ {
-    "name" : "Ni",
-    "photo" : "https://www.goldenglobes.com/sites/default/files/articles/cover_images/constance_wu-ag-20180806_zap_g203_037.pre_.jpeg",
+    "name" : "Ni Natsir",
+    "photo" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmGAJjx1M3kH7gblcgrYUHLBxLhDbp5_KRTNSWoieJA24rNX9f",
     "scores" : [5,4,2,5,1,1,2,1,3,5]
 }, {
-    "name" : "Alex",
-    "photo" : "",
+    "name" : "Alex Srikanth",
+    "photo" : "https://www.kreedon.com/wp-content/uploads/2018/07/kidambi.jpg",
     "scores" : [3,4,2,4,4,1,1,2,3,4]
 }, {
-    "name" : "Patrick Kim",
-    "photo" : "",
+    "name" : "Patrick Hidayat",
+    "photo" : "https://badmintonupdate.files.wordpress.com/2013/06/wpid-img-20130607-wa00003.jpg",
     "scores" : [5,5,5,5,5,5,5,5,5,5]
 }, {
-    "name" : "Sri",
-    "photo" : "",
+    "name" : "Sri Verma",
+    "photo" : "https://images.news18.com/ibnlive/uploads/2016/11/sameer-verma-new-getty.gif",
     "scores" : [5,2,4,3,3,2,1,3,3,4]
 }, {
-    "name" : "Tom",
-    "photo" : "",
+    "name" : "Tom Gunawan",
+    "photo" : "http://www.yonexusa.com/_assets/images/tony-gunawan-main(1).jpg",
     "scores" : [4,1,5,3,4,5,2,3,4,5]
 }, {
-    "name" : "Brenda",
-    "photo" : "https://www.google.com/imgres?imgurl=https%3A%2F%2Fsmilingbonita.files.wordpress.com%2F2011%2F04%2F10122501.jpg&imgrefurl=https%3A%2F%2Fsmilingbonita.wordpress.com%2F2011%2F04%2F19%2Fchinese-actress%2F&docid=90V0Ps8l0F2dAM&tbnid=HA2LusUdikTZ7M%3A&vet=10ahUKEwjy97r6nI3hAhUJsp4KHai1ChEQMwiaASg2MDY..i&w=1280&h=960&bih=512&biw=840&q=famous%20chinese%20actress&ved=0ahUKEwjy97r6nI3hAhUJsp4KHai1ChEQMwiaASg2MDY&iact=mrc&uact=8",
-    "scores" : [4,1,5,3,4,5,2,3,4,5]
+    "name" : "Brenda Ying",
+    "photo" : "http://www1.pictures.zimbio.com/gi/Tai+Tzu+Ying+Perodua+Malaysia+Masters+2018+54hvNaZ3D0jl.jpg",
+    "scores" : [4,2,3,3,1,5,2,3,4,5]
 }, {
-    "name" : "Brian",
-    "photo" : "",
-    "scores" : [3,4,2,4,4,1,1,2,3,4]
+    "name" : "Brian Lord Minton",
+    "photo" : "https://vignette.wikia.nocookie.net/summertons/images/5/51/Minton.jpg/revision/latest?cb=20170625165023",
+    "scores" : [3,4,2,4,4,1,1,2,4,1]
 }, {
-    "name" : "Ganesh",
-    "photo" : "",
-    "scores" : [5,4,2,5,1,1,2,1,3,5]
+    "name" : "Ganesh T'Challa",
+    "photo" : "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2018/04/avengers-infinity-war-black-panther.jpg",
+    "scores" : [5,1,2,3,1,1,2,1,3,5]
 }, {
-    "name" : "Cham",
-    "photo" : "",
+    "name" : "Cham Dan",
+    "photo" : "http://www.badmintonplanet.com/wp-content/uploads/2018/09/09-14-2018-badminton-news-lin-dan-japan-open.jpg",
     "scores" : [1,5,1,3,5,1,4,3,2,1]
 }, {
-    "name" : "Kristine",
-    "photo" : "",
+    "name" : "Kristine Wang",
+    "photo" : "https://s.yimg.com/ny/api/res/1.2/psOXYdVQ7aDNlzhTKrCeyg--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9MTI4MDtoPTk2MDtpbD1wbGFuZQ--/http://l.yimg.com/os/156/2012/08/03/149733319-10-jpg_110609.jpg",
     "scores" : [1,3,3,1,2,4,4,1,3,1]
 }, {
-    "name" : "Max",
-    "photo" : "",
-    "scores" : [5,2,4,3,3,2,1,3,3,4]
+    "name" : "Max CW Lee",
+    "photo" : "https://cdn.i-scmp.com/sites/default/files/styles/1200x800/public/d8/images/methode/2019/03/14/409287c8-456b-11e9-b5dc-9921d5eb8a6d_image_hires_141555.JPG?itok=9lhyQS_8",
+    "scores" : [4,2,4,3,3,2,1,1,3,5]
 }, {
-    "name" : "Sherry",
-    "photo" : "www.google.com",
+    "name" : "Sherry Intanon",
+    "photo" : "http://www.badmintonplanet.com/wp-content/uploads/2017/10/10-22-2017-badminton-news-Ratchanok-Intanon.jpg",
     "scores" : [1,1,1,1,1,1,1,1,1,1]
 }, {
-    "name" : "Amy",
-    "photo" : "",
+    "name" : "Amy Yamaguchi",
+    "photo" : "https://extranet.bwfbadminton.com/docs/players/96312/gallery/20161216_1809_WorldSuperseriesFinals2016_BPRS9640.jpg",
     "scores" : [2,3,5,5,3,5,2,1,3,2]
 }, {
-    "name" : "Jerry",
-    "photo" : "",
-    "scores" : [2,3,5,5,3,5,2,1,3,2]
+    "name" : "Jerry Momota",
+    "photo" : "https://assets.nst.com.my/images/articles/29_Kento_Momota_1527501147.jpg",
+    "scores" : [2,3,1,1,3,5,2,1,5,2]
 }];
 
 module.exports = friends;
